7aff968c8beb7fcbcd1b9cf9783f8b81d986d854
Sanchit0102 <137860157+Sanchit0102@users.noreply.github.com>
2023-12-25 16:38:37 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,12 +6,12 @@ class Config:
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "screenshot")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
     DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
-    MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
-    MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "921365334 563896360 974706111").split(" ")]
+    MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 10))
+    MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 6000))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
     SLOW_SPEED_DELAY = int(os.environ.get("SLOW_SPEED_DELAY", 5))
     HOST = os.environ.get("HOST", "")
